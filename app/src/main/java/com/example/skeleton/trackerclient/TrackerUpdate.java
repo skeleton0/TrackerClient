281bccff42d186dc7595c472c66c7dc2d95ea9e9
skeleton0 <34125010+skeleton0@users.noreply.github.com>
2019-03-11 14:53:24 +1100

    Add classes for requesting and storing tracker update

---
+++ app/src/main/java/com/example/skeleton/trackerclient/TrackerUpdate.java
@@ -0,0 +1,5 @@
+package com.example.skeleton.trackerclient;
+
+class TrackerUpdate {
+
+}
---
+++ app/src/main/java/com/example/skeleton/trackerclient/TrackerUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.example.skeleton.trackerclient;
+
+import android.os.AsyncTask;
+
+class TrackerUpdateRequest extends AsyncTask<Void, Void, TrackerUpdate> {
+    @Override
+    protected TrackerUpdate doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(TrackerUpdate trackerUpdate) {
+        super.onPostExecute(trackerUpdate);
+    }
+}
